bf0be980811f57af4b10c12a8a1930681d81804d
irahetAlberto <irahetalberto@gmail.com>
2023-06-01 16:53:22 +0200

    code modifie

--- dist/style/js/cards_script.js
+++ dist/style/js/cards_script.js
@@ -1,16 +1,11 @@
+const speakerCards = document.querySelectorAll('.speaker_card');
 
-let speaker_section = document.querySelector(".speaker_section") //écouter un clic sur tout
-speaker_section.addEventListener("click", function (e) {// détecter le clic sur l'élément
-let mySection = e.target.closest(".speaker_card") //cherche le parent de type "section" de l'élément réellement cliqué
-//console.log(mySection.classList)
-if(mySection.classList.contains("speaker_card-active")) { //classlist liste des classes à l'élément -> speaker active
-  mySection.classList.remove("speaker_card-active")// retire classe
-}
-  else { // ajout de la classe mais avant vérifier si élément ouvert avant d'ouvrir élement sur lequel on clic
-   if(document.querySelector(".speaker_card-active")) {document.querySelector(".speaker_card-active").classList.remove("speaker_card-active")} //si un actif, on le retire
-    mySection.classList.add("speaker_card-active")
-  } 
-})
+speakerCards.forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.toggle('active');
+  });
+});
+
 
 //->> jQuery
 //$(function(){
