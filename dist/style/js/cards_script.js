b34c1451720b7196801c56e2f57c8efb186fa2b0
irahetAlberto <irahetalberto@gmail.com>
2023-06-01 15:42:41 +0200

    still working on the cards java changes bg

--- dist/style/js/cards_script.js
+++ dist/style/js/cards_script.js
@@ -3,7 +3,7 @@ let speaker_section = document.querySelector(".speaker_section") //écouter un c
 speaker_section.addEventListener("click", function (e) {// détecter le clic sur l'élément
 let mySection = e.target.closest(".speaker_card") //cherche le parent de type "section" de l'élément réellement cliqué
 //console.log(mySection.classList)
-if(mySection.classList.contains(".speaker_card-active")) { //classlist liste des classes à l'élément -> speaker active
+if(mySection.classList.contains("speaker_card-active")) { //classlist liste des classes à l'élément -> speaker active
   mySection.classList.remove("speaker_card-active")// retire classe
 }
   else { // ajout de la classe mais avant vérifier si élément ouvert avant d'ouvrir élement sur lequel on clic
